d49381e17126c6b23f7d0c088bf3b4329ac6b4e6
ivapark <cmpark1656@gmail.com>
2025-01-21 20:32:15 -0500

    okay i fixed it

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
@@ -13,23 +14,8 @@ const theme = createTheme({
     typography: {
       fontFamily: "'Playfair Display', serif", // Updated fontFamily
     },
-=======
-// src/styles/theme.ts
-import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: "'Roboto', 'Arial', sans-serif",
-  },
-  palette: {
-    primary: {
-      main: '#5A2750',
-    },
-    secondary: {
-      main: '#F5E9E8',
-    },
-  },
->>>>>>> e96a7700e0a8060bf00f796ca2b1d0dd0e547844
 });
 
 export default theme;
+
